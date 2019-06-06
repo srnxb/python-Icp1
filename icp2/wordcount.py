6d474424808740f8d70769e68175c00c31fd7afd
srnxb <47068529+srnxb@users.noreply.github.com>
2019-06-06 12:54:35 -0500

    Update wordcount.py

--- icp2/wordcount.py
+++ icp2/wordcount.py
@@ -2,10 +2,10 @@ file = open('text.txt')
 wordcount = {}
 
 for word in file.read().split():
-    if word not in wordcount:
-        wordcount[word] = 1
+    if word.lower() not in wordcount:
+        wordcount[word.lower()] = 1
     else:
-        wordcount[word] += 1
+        wordcount[word.lower()] += 1
 
 for k, v in wordcount.items():
     print (k, v)
